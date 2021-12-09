c3d70281de778dad9b0b95edb8daa6ec27b1969b
Christine Lin <73617227+christinealin7771@users.noreply.github.com>
2021-12-09 00:56:14 -0500

    Added BellMan Ford

--- Jeopardy.cpp
+++ Jeopardy.cpp
@@ -20,7 +20,7 @@ unordered_map<int, vector<string>> readFile()
     string answer;
     string index;
     string line;
-    ifstream readFile("Jeopardy_cleaned_tsv.tsv");
+    ifstream readFile("Jeopardy_cleaned_tsv_updated.tsv");
 
 
     getline(readFile, line);
@@ -120,14 +120,62 @@ vector<int> dijkstraAlg (int startIndex, int endIndex, vector<pair<int,int>> adj
     if(startIndex != endIndex)
       previous[endIndex].erase(previous[endIndex].begin());
     return previous[endIndex];
-
 }
+
+vector<int> Bellman_Ford(vector<pair<int, int>> graph[], int source, int destination, int adjSize)
+{
+	int vectors = adjSize;
+	vector<int> distances;
+  vector<int> pathIndex[100000];
+
+	for (int i = 0; i < vectors; i++)
+  {
+    distances.push_back(INT_MAX);
+    pathIndex[i].push_back(source);
+  }
+		
+	distances[source] = 0;
+
+  //number of vertices - 1 loops
+	for (int i = 0 ; i < vectors - 1; i++)
+	{
+    //they have it where you get the source of an index
+		for (int j = source; j < source + vectors; j++)
+		{
+      for(int k = 0; k < graph[j].size(); k++)
+      {
+        int start = j; // source== index 0
+        int end = graph[j][k].first;
+        int weight = graph[j][k].second;
+        if(pathIndex[end][k] == source && end != source)
+              pathIndex[end][0] = start;
+        if (distances[start] != INT_MAX && distances[start] + weight < distances[end])
+        {
+          pathIndex[end] = pathIndex[start];
+          pathIndex[end].push_back(start);
+          distances[end] = distances[start] + weight;
+
+
+        }
+          
+      }
+			
+		}
+	}
+  if(source != destination)
+      pathIndex[destination].erase(pathIndex[destination].begin());
+
+	return pathIndex[destination];
+}
+
+
 
 
 
 int main()
 {
     unordered_map<int, vector<string>> data = readFile();
+
 
     //array of vector storing pairs
     
@@ -138,9 +186,9 @@ int main()
 //        addEdge(adj, i, rand() % 100000, (rand() % 20) + 1);
 //        addEdge(adj, i, rand() % 100000, (rand() % 20) + 1);
 //        addEdge(adj, i, rand() % 100000, (rand() % 20) + 1);
+//        edgeNum += 6;
 //    }
 
-    //testing purposes
      vector<pair<int, int>> adj[7];
      addEdge(adj, 0, 1, 1);
      addEdge(adj,0, 3, 3);
@@ -153,12 +201,27 @@ int main()
      addEdge(adj, 5, 3, 4);
      addEdge(adj, 3, 6, 4);
 
+    //used to test bellman ford
+    // vector<pair<int, int>> adj[7];
+    //  addEdge2(adj, 0, 1, 3);
+    //  addEdge2(adj,0, 2, 8);
+    //  addEdge2(adj, 1, 4, 6);
+    //  addEdge2(adj, 1, 3, 6);
+    //  addEdge2(adj, 1, 2, 7);
+    //  addEdge2(adj, 3, 1, 5);
+    //  addEdge2(adj, 3, 2, 9);
+    //  addEdge2(adj, 4, 3, 1);
+
+     //a=0, b= 1, c= 2, d=3, e=4
+
     //change the index
 //    int startIndex = 10;
 //    int endIndex = 10000;
     int startIndex = 0;
     int endIndex = 6;
 
+  
+    vector<int> bellManFordIndex = Bellman_Ford(adj, startIndex, endIndex, 7);
 
     //use the algorithm to find all path and use the end index to output the result of this path as questions
     //menu driven program
@@ -186,6 +249,7 @@ int main()
 
             if (option == 1) {
                 vector<int> index = dijkstraAlg(startIndex, endIndex, adj);
+                //comment out later
                 cout << index.size() << endl;
                 cout << "How many questions would you like?" << endl;
                 cin >> option;
